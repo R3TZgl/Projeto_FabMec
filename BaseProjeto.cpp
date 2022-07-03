503adac4f7c9d0d0ed6ca1e829e0ce21ce6e9692
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2022-07-03 13:39:05 -0300

    Som

--- BaseProjeto.cpp
+++ BaseProjeto.cpp
@@ -1,11 +1,16 @@
 #include <AudioFrequencyMeter.h>
 AudioFrequencyMeter meter;
+const int sensor = 10;
+const int led = 11;
 
 void setup(){
     Serial.begin(115200);
     Serial.printIn("started");
     meter.setBandwidth(70.00, 1500);
     meter.begin(A0, 45000);
+    
+    pinMode(sensor, INPUT);
+    pinMode(led, OUTPUT);
 }
 
 void loop(){
@@ -13,5 +18,16 @@ void loop(){
     if(frequencia > 0){
         print(frequencia);
         printIn("Hz");
+    }
+    
+    int som = analogRead(sensor);
+    Serial.printIn(som);
+    delay(50);
+    
+    if(som > 300){
+        digitalWrite(led, HIGH);
+        delay(200);
+    }else{
+        digitalWrite(led, LOW);
     }
 }
