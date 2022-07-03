b33e5f70b87ba847601749848f5f97e25b50835e
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2022-07-03 14:16:44 -0300

    Notas

--- BaseProjeto.cpp
+++ BaseProjeto.cpp
@@ -10,23 +10,29 @@ const int led = 11;
 Servo servo;
 int angulo = 0;
 
-const int e = 329;
+const int e = 330;
+const int b = 247;
+const int g = 196;
+const int d = 146;
+const int a = 110;
+const int E = 82;
 
 void nota(nota, frequencia, som, angulo){
-    if(som > 300){
-        while(frequencia < nota -2){
-            
-            angulo += 1;
-            servo.write(angulo);
-        }    
-        while(frequência > nota +2){
-            angulo -= 1;
-            servo.write(angulo);
+    while(!(nota - 2) < frequencia < (nota + 2){
+        if(som > 300){
+            while(frequencia < nota - 2){
+                // da pra simplificar só com o if
+                angulo += 1;
+                servo.write(angulo);
+            }    
+            while(frequência > nota + 2){
+                angulo -= 1;
+                servo.write(angulo);
+            }
         }
     }
     return angulo;
 }
-  
 
 
 
