fa84de53547109fc481ebea7c82c2e6a21a33a4b
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2022-07-03 12:01:14 -0300

    Frequência

--- BaseProjeto.cpp
+++ BaseProjeto.cpp
@@ -1,7 +1,17 @@
-void setup(){
+#include <AudioFrequencyMeter.h>
+AudioFrequencyMeter meter;
 
+void setup(){
+    Serial.begin(115200);
+    Serial.printIn("started");
+    meter.setBandwidth(70.00, 1500);
+    meter.begin(A0, 45000);
 }
 
 void loop(){
-    
+    float frequencia = meter.getFrequency();
+    if(frequencia > 0){
+        print(frequencia);
+        printIn("Hz");
+    }
 }
